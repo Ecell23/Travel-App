49390647f9ed6a13404c1db315be5542dad03b62
Rishav Kumar Sinha <sinharishavkumar@gmail.com>
2025-01-29 19:18:53 +0530

    Login Functionality added

--- startup/userRoutes.js
+++ startup/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const signup = require('../routes/user/signup');
 const sendOtp = require('../routes/user/send_otp');
+const login = require('../routes/user/login');
 
 module.exports = function(app){
     app.use('/api/user/signUp',signup);
     app.use('/api/user/send_otp',sendOtp);
+    app.use('/api/user/login',login);
 }
